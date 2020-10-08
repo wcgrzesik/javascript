3a629336dc59677097448b77a748c33e3b927aea
Wojciech Grzesik <wcgrzesikslzb@gmail.com>
2020-10-08 22:46:00 +0200

    po godzinie z kol. to jedyne co udalo sie zrobic gdzie javascript dziala choc nie wiemy dlaczego nadal

--- timer.js
+++ timer.js
@@ -1,16 +1,3 @@
-function odliczanie()
-{
-   var today = new Date();
-   var day = today.getDate();
-   var month = today.getMonth()+1;
-   var year = today.getFullYear();
-   var hours = today.getHours();
-   if(hours<10) hours = "0" +today;
-   var minutes = today.getMinutes();
-   if(hours<10) today = "0" +hours;
-   var seconds = today.getSeconds();
-   if(hours<10) seconds = "0" +Seconds;
-
-   document.getElementByClass("timer").innerHTML = day+"/"+month+"/"+year+"|"+hour+":"+minutes+":"+seconds;
-   setTimeout("odliczanie()",1000ms);
+function odliczanie() {
+    document.getElementsByClassName("timer")[0].innerHTML = "XD";
 }
