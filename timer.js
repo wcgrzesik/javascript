102c9d1bf2dac4eac2a61acaaba4305057e564ff
Wojciech Grzesik <wcgrzesikslzb@gmail.com>
2020-10-09 15:13:57 +0200

    zegarek

--- timer.js
+++ timer.js
@@ -1,3 +1,13 @@
 function odliczanie() {
-    document.getElementsByClassName("timer")[0].innerHTML = "XD";
+    const date = new Date();
+
+    var rok = date.getFullYear();
+    var miesiac = date.getMonth();
+    var dzien = date.getDay();
+    var godzina = date.getHours();
+    var minuta = date.getMinutes();
+    var sekunda = date.getSeconds();
+
+    document.getElementsByClassName("timer")[0].innerHTML = `Dzien: ${dzien}, miesiac: ${miesiac}, rok: ${rok}, godzin: ${godzina}, minut: ${minuta}, sekund: ${sekunda}`;
+    setTimeout(odliczanie, 1000);
 }
