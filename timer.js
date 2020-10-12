8c06121d1ac5ee975bdf69dbeec4ca837083b102
Wojciech Grzesik <wcgrzesikslzb@gmail.com>
2020-10-12 14:38:47 +0200

    po wielu bledach, udalo sie

--- timer.js
+++ timer.js
@@ -8,6 +8,15 @@ function odliczanie() {
     var minuta = date.getMinutes();
     var sekunda = date.getSeconds();
 
-    document.getElementsByClassName("timer")[0].innerHTML = `Dzien: ${dzien}, miesiac: ${miesiac}, rok: ${rok}, godzin: ${godzina}, minut: ${minuta}, sekund: ${sekunda}`;
+    document.getElementsByClassName("timer")[0].innerHTML = `Dzień: ${dzien}, miesiąc: ${miesiac}, rok: ${rok}, godzin: ${godzina}, minut: ${minuta}, sekund: ${sekunda}`;
     setTimeout(odliczanie, 1000);
+}
+function sprawdz()
+{
+    var liczba = document.getElementById("pole").value;
+    
+    if (liczba>0) document.getElementById("wynik").innerHTML="dodatnia";
+    else if (liczba<0) document.getElementById("wynik").innerHTML="ujemna";
+    else if (liczba==0) document.getElementById("wynik").innerHTML="zero";
+    else document.getElementById("wynik").innerHTML="to nie jest liczba";
 }
