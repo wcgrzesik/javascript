69b121e5410a7434cad287a236fd6cdd19f70934
Wojciech Grzesik <wcgrzesikslzb@gmail.com>
2020-10-14 15:44:47 +0200

    kilka skryptów

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+function onload()
+{
+  slider();
+  odliczanie();
+}
 function odliczanie() {
     const date = new Date();
 
@@ -11,6 +16,15 @@ function odliczanie() {
     document.getElementsByClassName("timer")[0].innerHTML = `Dzień: ${dzien}, miesiąc: ${miesiac}, rok: ${rok}, godzin: ${godzina}, minut: ${minuta}, sekund: ${sekunda}`;
     setTimeout(odliczanie, 1000);
 }
+var number = Math.floor(Math.random()*5)+1;
+function slider()
+{
+  number++; if (number>5) number=1;
+var img = "<img src=\"slajd/slajd" + number + ".png\"/>";
+  document.getElementById("slider").innerHTML = img;
+  setTimeout("slider()", 5000);
+
+}
 function sprawdz()
 {
     var liczba = document.getElementById("pole").value;
@@ -19,4 +33,20 @@ function sprawdz()
     else if (liczba<0) document.getElementById("wynik").innerHTML="ujemna";
     else if (liczba==0) document.getElementById("wynik").innerHTML="zero";
     else document.getElementById("wynik").innerHTML="to nie jest liczba";
+}
+function zakres()
+{
+  var liczba1 = document.getElementById("pole1").value;
+  var liczba2 = document.getElementById("pole2").value;
+  var napis = "";
+  
+  for (i=liczba1; i<=liczba2; i++)
+  {
+      napis = napis + i + "";
+  }
+  for (i=liczba1; i>=liczba2; i--)
+  {
+    napis = napis + i + "";
+  }
+document.getElementById("wynik2").innerHTML = napis;
 }
